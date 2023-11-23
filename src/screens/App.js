79ec792d559c1d76118c27ed077c97ad96ae394d
Aleksandra <elinda100@gmail.com>
2023-11-23 19:58:40 +0100

    added react navigation library

--- src/screens/App.js
+++ src/screens/App.js
@@ -4,11 +4,13 @@ import { ApplicationProvider, Layout, Text } from "@ui-kitten/components";
 import Home from "./Home";
 import Blocks from "./Blocks";
 import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default () => (
-  <ApplicationProvider {...eva} theme={eva.light}>
-    <StatusBar style="auto" />
-    {/* black status bar */}
-    <Blocks />
-  </ApplicationProvider>
+  <NavigationContainer>
+    <ApplicationProvider {...eva} theme={eva.light}>
+      <StatusBar style="auto" />
+      <Home />
+    </ApplicationProvider>
+  </NavigationContainer>
 );
