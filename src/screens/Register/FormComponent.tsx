3f95a4e7ea87d34604100e7a26f7ebf621694097
Aleksandra <elinda100@gmail.com>
2023-11-25 17:23:20 +0100

    added register page

---
+++ src/screens/Register/FormComponent.tsx
@@ -0,0 +1,77 @@
+import React from "react";
+import { View } from "react-native";
+import { Text, Input } from "@ui-kitten/components";
+import { Controller } from "react-hook-form";
+import { Control, FieldErrors } from "react-hook-form";
+
+type FormData = {
+  login: string;
+  password: string;
+  confirmPassword: string;
+};
+
+type FormComponentProps = {
+  control: Control<FormData>;
+  errors: FieldErrors<FormData>;
+};
+
+export const FormComponent: React.FC<FormComponentProps> = ({
+  control,
+  errors,
+}) => (
+  <View className="flex flex-grow justify-around py-10 h-1/3">
+    <Controller
+      control={control}
+      render={({ field }) => (
+        <Input
+          label="Login"
+          size="large"
+          onChangeText={field.onChange}
+          value={field.value}
+        />
+      )}
+      name="login"
+    />
+    {errors && errors.login && (
+      <Text style={{ color: "red", textAlign: "center", marginBottom: "2%" }}>
+        Login jest wymagany
+      </Text>
+    )}
+    <Controller
+      control={control}
+      render={({ field }) => (
+        <Input
+          label="Hasło"
+          size="large"
+          secureTextEntry
+          onChangeText={field.onChange}
+          value={field.value}
+        />
+      )}
+      name="password"
+    />
+    {errors && errors.password && (
+      <Text style={{ color: "red", textAlign: "center", marginBottom: "2%" }}>
+        Hasło jest wymagane
+      </Text>
+    )}
+    <Controller
+      control={control}
+      render={({ field }) => (
+        <Input
+          label="Potwierdź hasło"
+          size="large"
+          secureTextEntry
+          onChangeText={field.onChange}
+          value={field.value}
+        />
+      )}
+      name="confirmPassword"
+    />
+    {errors && errors.confirmPassword && (
+      <Text style={{ color: "red", textAlign: "center", marginBottom: "2%" }}>
+        Potwierdzenie hasła jest wymagane
+      </Text>
+    )}
+  </View>
+);
--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,6 +1,63 @@
-import { Text } from "@ui-kitten/components";
+import React from "react";
+import { Image, View, Dimensions } from "react-native";
+import { Layout, Text, Button } from "@ui-kitten/components";
+import { object, string } from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm, SubmitHandler, Control } from "react-hook-form";
+import { FormComponent } from "./FormComponent";
 
-//Register Page
-export default function Register({ navigation }: any) {
-  return <Text>Register page</Text>;
+type FormData = {
+  login: string;
+  password: string;
+  confirmPassword: string;
+};
+
+const windowWidth = Dimensions.get("window").width;
+
+// Yup schema
+const schema = object().shape({
+  login: string().required("Login jest wymagany"),
+  password: string().required("Hasło jest wymagane"),
+  confirmPassword: string()
+    .required("Konieczne jest potwierdzenie hasła")
+    .test("passwords-match", "Hasła muszą do siebie pasować", function (value) {
+      return this.parent.password === value;
+    }),
+});
+
+// MAIN PAGE
+export default function Home({ navigation }: any) {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: yupResolver(schema),
+    defaultValues: {
+      login: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<FormData> = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <Layout style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <View className="flex justify-between h-2/3 w-3/4">
+        <View>
+          <Text style={{ textAlign: "center" }} category="h2">
+            Rejestracja
+          </Text>
+        </View>
+
+        <FormComponent control={control as Control<FormData>} errors={errors} />
+        <View className="pb-5">
+          <Button onPress={handleSubmit(onSubmit)}>Zarejestruj się</Button>
+        </View>
+      </View>
+    </Layout>
+  );
 }
