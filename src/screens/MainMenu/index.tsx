92fd420c356902f283e7d6c12df7a2282b5d6be0
Aleksandra <elinda100@gmail.com>
2023-11-26 18:24:28 +0100

    changed styles for button

--- src/screens/MainMenu/index.tsx
+++ src/screens/MainMenu/index.tsx
@@ -25,7 +25,6 @@ export default function MainMenu({ navigation }: any) {
         <Button
           style={{
             ...styles.button,
-            backgroundColor: "#e1f0fa",
           }}
           size="giant"
           appearance="outline"
