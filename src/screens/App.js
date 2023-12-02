9c1a382c350a60694891a9a0bad14141782f2614
Aleksandra <elinda100@gmail.com>
2023-12-02 18:59:42 +0100

    added evaIcons to project

--- src/screens/App.js
+++ src/screens/App.js
@@ -1,13 +1,15 @@
 // App.js
 import React from "react";
 import * as eva from "@eva-design/eva";
-import { ApplicationProvider } from "@ui-kitten/components";
+import { ApplicationProvider, IconRegistry } from "@ui-kitten/components";
 import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
 import Navigator from "../components/Navigator";
+import { EvaIconsPack } from "@ui-kitten/eva-icons";
 
 export default () => (
   <NavigationContainer>
+    <IconRegistry icons={EvaIconsPack} />
     <ApplicationProvider {...eva} theme={eva.light}>
       <StatusBar style="auto" />
       <Navigator />
