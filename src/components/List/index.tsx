5f77050d0b5d17a0a79854aedb8762b37994bc44
Aleksandra <elinda100@gmail.com>
2023-12-02 19:00:37 +0100

    styled & made myProject screen

---
+++ src/components/List/index.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+import {
+  Divider,
+  Icon,
+  IconElement,
+  List,
+  ListItem,
+} from "@ui-kitten/components";
+
+export interface IListItem {
+  title: string;
+}
+
+interface ListDividersShowcaseProps {
+  items: IListItem[];
+}
+
+export const ListDividersShowcase = ({
+  items,
+}: ListDividersShowcaseProps): React.ReactElement => {
+  const renderItemIcon = (props: any): IconElement => (
+    <Icon
+      {...props}
+      style={[props.style, { width: 25, height: 25 }]}
+      name="trash-outline"
+    />
+  );
+
+  const renderItem = ({
+    item,
+    index,
+  }: {
+    item: IListItem;
+    index: number;
+  }): React.ReactElement => (
+    <ListItem
+      title={`${index + 1}. ${item.title}`}
+      accessoryRight={renderItemIcon}
+      style={styles.items}
+    />
+  );
+
+  return (
+    <List
+      style={styles.container}
+      data={items}
+      ItemSeparatorComponent={Divider}
+      renderItem={renderItem}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    height: "100%",
+  },
+  items: {
+    paddingVertical: 30,
+  },
+  icon: {
+    width: 32,
+    height: 32,
+  },
+});
--- src/screens/MyProjects/index.tsx
+++ src/screens/MyProjects/index.tsx
@@ -1,9 +1,21 @@
-import { View, Text } from "react-native";
+import { useEffect, useState } from "react";
+import { View } from "react-native";
+import { ListDividersShowcase, IListItem } from "../../components/List";
 
 export default function MyProjects() {
+  const [projects, setProjects] = useState<IListItem[]>([]);
+
+  useEffect(() => {
+    const exampleProjects = [
+      { title: "jeden" },
+      { title: "dwa" },
+      { title: "trzy" },
+    ];
+    setProjects(exampleProjects);
+  }, []);
   return (
     <View>
-      <Text>MyProjects</Text>
+      <ListDividersShowcase items={projects} />
     </View>
   );
 }
