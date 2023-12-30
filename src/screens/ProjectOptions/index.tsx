ad6e974a7b2f85091d4e90fbce2ddf9603abace9
Aleksandra <elinda100@gmail.com>
2023-12-30 17:42:40 +0100

    added ProjectOptions screen:

--- src/screens/ProjectOptions/index.tsx
+++ src/screens/ProjectOptions/index.tsx
@@ -1,9 +1,36 @@
-import { Text } from "react-native";
+import { Dimensions, Image, StyleSheet, View } from "react-native";
+import { Button } from "@ui-kitten/components";
 
-export default function ProjectOptions() {
+export default function ProjectOptions({ navigation }: any) {
   return (
-    <>
-      <Text>ProjectOptions</Text>
-    </>
+    <View className="w-full h-full flex justify-around pb-14 bg-white">
+      <View className="h-1/2 py-5 w-2/3 justify-around mx-auto">
+        <Button
+          style={styles.button}
+          size="giant"
+          appearance="filled"
+          onPress={() => navigation.navigate("NewProject")}
+        >
+          Wyświetl schemat
+        </Button>
+        <Button
+          style={{
+            ...styles.button,
+          }}
+          size="giant"
+          appearance="outline"
+          onPress={() => navigation.navigate("MyProjects")}
+        >
+          Eksport do kodu
+        </Button>
+      </View>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  button: {
+    borderRadius: 15,
+    height: 100,
+  },
+});
