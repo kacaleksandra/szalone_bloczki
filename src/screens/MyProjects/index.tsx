853aa83b6619cf8e7cd27c3fcba3fa2c9ff0ec01
Aleksandra <elinda100@gmail.com>
2024-01-20 16:22:47 +0100

    added loading to fetching schemtics

--- src/screens/MyProjects/index.tsx
+++ src/screens/MyProjects/index.tsx
@@ -3,9 +3,12 @@ import { View } from "react-native";
 import { ListDividersShowcase, IListItem } from "../../components/List";
 import { getApiURL } from "../../composables/getApiURL";
 import { getToken } from "../../composables/getToken";
+import { set } from "react-hook-form";
+import { Text } from "@ui-kitten/components";
 
 export default function MyProjects() {
   const [projects, setProjects] = useState<IListItem[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
   const token = getToken();
 
   const fetchProjects = async () => {
@@ -24,6 +27,7 @@ export default function MyProjects() {
       const data = await response.json();
       console.log(data);
       setProjects(data);
+      setIsLoading(false);
     } catch (error) {
       console.error("Error fetching projects");
     }
@@ -35,6 +39,11 @@ export default function MyProjects() {
 
   return (
     <View>
+      {isLoading && (
+        <View className="h-1/3 flex justify-center bg-white">
+          <Text style={{ textAlign: "center" }}>Loading...</Text>
+        </View>
+      )}
       <ListDividersShowcase items={projects} set={setProjects} />
     </View>
   );
