ef67afe9e5be0b849c3b6b360a032d39dfa9892b
Denis Poczęty <49251639+Deniorrr@users.noreply.github.com>
2024-01-23 00:31:20 +0100

    kod shrodingera: jednocześnie działa i nie działa

--- src/screens/Blocks/index.tsx
+++ src/screens/Blocks/index.tsx
@@ -5,14 +5,17 @@ import { WebView } from "react-native-webview";
 import axios from "axios";
 import { useRoute } from "@react-navigation/native";
 import { getApiURL } from "../../composables/getApiURL";
+import ViewShot from "react-native-view-shot";
+import { Buffer } from "buffer";
+import { getToken } from "../../composables/getToken";
 
 //Block diagram view
 export default function Blocks() {
-  const HEIGHT = 10;
-  const WIDTH = 5;
-  const SIZE = 130;
-  const BORDERWIDTH = 1;
+  // const WIDTH = 5;
+  // const SIZE = 130;
+  // const BORDERWIDTH = 1;
   const route = useRoute();
+  const token = getToken();
   // const [tableContent, setTableContent] = useState(
   //   Array.from({ length: HEIGHT }, (v) =>
   //     Array.from({ length: WIDTH }, (v) => 0)
@@ -27,42 +30,45 @@ export default function Blocks() {
   //   setTableContent(helper);
   // };
 
-  const uploadImage = async (imageUri: String) => {
+  const uploadImage = async (imageUri: string) => {
     try {
-      console.log(imageUri);
-      const formData = new FormData();
-      formData.append("image", {
-        uri: imageUri,
-        name: "photo.jpg",
-        type: "image/jpg", // Adjust the type as per your image format
-      });
-
-      const apiURL = getApiURL();
-
-      await axios
-        .post(`${apiURL}convert`, formData, {
-          headers: {
-            "Content-Type": "multipart/form-data",
-          },
-        })
-        .then((response) => {
-          console.log(response.data);
-        });
+      const image = await fetch(imageUri);
+      const imageBlob = await image.blob();
+      const reader = new FileReader();
+      reader.readAsDataURL(imageBlob);
+      reader.onloadend = async function () {
+        const base64data = reader.result;
+        const apiURL = getApiURL();
+        await axios
+          .post(
+            `${apiURL}convert`,
+            { data: base64data },
+            {
+              headers: {
+                Authorization: `Bearer ${token}`,
+                "Content-Type": "application/json",
+              },
+            }
+          )
+          .then((response) => {
+            console.log(response.data);
+          });
+      };
     } catch (error) {
       console.error("Upload failed:", error);
     }
   };
 
   //to pewnie do innego pliku pójdzie
-  const styles = StyleSheet.create({
-    container: {
-      width: WIDTH * SIZE + 2 * BORDERWIDTH,
-      borderColor: "blue",
-      borderWidth: 1,
-      margin: 5,
-      marginTop: 40,
-    },
-  });
+  // const styles = StyleSheet.create({
+  //   container: {
+  //     width: WIDTH * SIZE + 2 * BORDERWIDTH,
+  //     borderColor: "blue",
+  //     borderWidth: 1,
+  //     margin: 5,
+  //     marginTop: 40,
+  //   },
+  // });
 
   const viewShot = useRef<any>(null);
   const [uri, setUri] = useState<string>("");
@@ -104,18 +110,21 @@ export default function Blocks() {
         maximumZoomScale={2}
         minimumZoomScale={0.5}
         contentContainerStyle={styles.container}
+      >*/}
+      <ViewShot
+        ref={viewShot}
+        options={{ format: "jpg", quality: 1 }}
+        style={{ width: "100%", height: "90%" }}
       >
-        <ViewShot ref={viewShot} options={{ format: "jpg", quality: 0.9 }}> */}
-      <WebView
-        ref={webViewRef}
-        source={{
-          //uri: "192.168.5.9:3000",
-          uri: "https://deniorrr.github.io/bloczki-diagram/",
-        }}
-        onLoadEnd={sendObjectToWebView}
-      />
-      {/* </ViewShot>
-      </ScrollView> */}
+        <WebView
+          ref={webViewRef}
+          source={{
+            uri: "https://deniorrr.github.io/bloczki-diagram/",
+          }}
+          onLoadEnd={sendObjectToWebView}
+        />
+      </ViewShot>
+      {/*</ScrollView> */}
       <Button onPress={captureScreen} size="large">
         Wygeneruj PDF
       </Button>
